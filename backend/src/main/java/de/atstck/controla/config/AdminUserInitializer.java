1167a6fbcdf4362faa25ead9f52796e89f8aeceb
Johannes Hinkov <johannes.hinkov@gmail.com>
2026-01-16 16:07:33 +0100

    refactor: remove unused logger imports in AdminUserInitializer

--- backend/src/main/java/de/atstck/controla/config/AdminUserInitializer.java
+++ backend/src/main/java/de/atstck/controla/config/AdminUserInitializer.java
@@ -4,8 +4,6 @@ import de.atstck.controla.security.CryptoService;
 import de.atstck.controla.user.User;
 import de.atstck.controla.user.UserRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
